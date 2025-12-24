b297333ce031fb625ab1d308e0b6785797b36a95
look <nemoophilist@163.com>
2025-12-24 09:16:42 +0800

    rviz: dual_arm_demo package + urdf/meshes + publisher + launch

--- build/dual_arm_demo/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/look/ros2_ws/install/dual_arm_demo'
--- build/dual_arm_demo/share/dual_arm_demo/hook/pythonpath_develop.ps1
+++
@@ -1,3 +0,0 @@
-# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
-
-colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/home/look/ros2_ws/build/dual_arm_demo"
